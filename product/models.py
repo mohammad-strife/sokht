d83cc5b501500ea524c8230a2f93704911be8763
saei1d <hehehe3bar@gmail.com>
2024-12-13 02:01:44 +0330

    last

--- product/models.py
+++ product/models.py
@@ -19,19 +19,16 @@ class Product(models.Model):
     deleted = models.BooleanField(default=False)
 
 
-
-
 class Order(models.Model):
     fullname = models.CharField(max_length=100, blank=True, null=True)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     description = models.TextField()
     location = models.CharField(max_length=500, blank=True, null=True)
-    date = models.CharField(max_length=500,null=True, blank=True)
+    date = models.CharField(max_length=500, null=True, blank=True)
     status = models.CharField(max_length=100)
     cart = models.BooleanField(default=False)
     amount = models.IntegerField(null=True, blank=True)
     lat = models.FloatField(null=True, blank=True)
-    lon = models.FloatField(null=True,blank=True)
+    lon = models.FloatField(null=True, blank=True)
     quantity = models.IntegerField(null=True, blank=True)
-
